7268f28a1c168ec9efc4e9369473afe29f83834e
Johnny Chen <johnnychen94@hotmail.com>
2020-07-02 11:54:48 +0800

    reduce test stage dependency with ReferenceTests

--- test/criminisi.jl
+++ test/criminisi.jl
@@ -1,34 +1,34 @@
 @testset "Crimisini" begin
-  if visualtests
+    refdir = joinpath(datadir, "criminisi")
+
     # Diagonal
     img  = [i + j > 10 ? 0.1 : 0.2 for i in 1:10, j in 1:10]
     mask = [1.2cos(i) + i > j for i in 1:10, j in 1:10]
     out  = inpaint(img, mask, Criminisi(5,5))
-    @plottest plot_before_after(img,mask,out) joinpath(datadir,"Diagonal.png") !istravis
+    @test_reference joinpath(refdir, "Diagonal.png") Gray.(out)
 
     # Cross
     img = zeros(50,50) .+ 0.5
     for i in 1:50
-      img[i,i] = 1.0
-      img[i,50-i+1] = 1.0
+        img[i,i] = 1.0
+        img[i,50-i+1] = 1.0
     end
     mask = falses(size(img))
     mask[20:30,15:40] .= true
     out = inpaint(img, mask, Criminisi(9,9))
-    @plottest plot_before_after(img,mask,out) joinpath(datadir,"Cross.png") !istravis
+    @test_reference joinpath(refdir, "Cross.png") Gray.(out)
 
     # Blobs
     img  = Float64.(Gray.(testimage("blobs")))
     mask = falses(size(img))
     mask[50:150,50:150] .= true
     out = inpaint(img, mask, Criminisi(11,11))
-    @plottest plot_before_after(img,mask,out) joinpath(datadir,"Blobs.png") !istravis
+    @test_reference joinpath(refdir, "Blobs.png") Gray.(out)
 
     # LightHouse
     img  = Float64.(Gray.(testimage("lighthouse")))
     mask = falses(size(img))
     mask[50:350,300:400] .= true
     out = inpaint(img, mask, Criminisi(30,30))
-    @plottest plot_before_after(img,mask,out) joinpath(datadir,"LightHouse.png") !istravis
-  end
+    @test_reference joinpath(refdir, "LightHouse.png") Gray.(out)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,30 +1,8 @@
 using ImageInpainting
-using ColorTypes
-using TestImages
-using Plots, VisualRegressionTests
-using Test, Pkg
+using ImageCore
+using TestImages, Test, ReferenceTests
 
-# workaround GR warnings
-ENV["GKSwstype"] = "100"
-
-# environment settings
-islinux = Sys.islinux()
-istravis = "TRAVIS" ∈ keys(ENV)
-datadir = joinpath(@__DIR__,"data")
-visualtests = !istravis || (istravis && islinux)
-if !istravis
-  Pkg.add("Gtk")
-  using Gtk
-end
-
-# helper functions
-function plot_before_after(before, mask, after)
-  gr(size=(800,400),yflip=true,colorbar=false,ticks=false)
-  B = copy(before); B[mask] .= NaN
-  p1 = heatmap(B,title="before")
-  p2 = heatmap(after,title="after")
-  plot(p1, p2, aspect_ratio=:equal)
-end
+datadir = joinpath(@__DIR__, "data")
 
 # list of tests
 testfiles = [
