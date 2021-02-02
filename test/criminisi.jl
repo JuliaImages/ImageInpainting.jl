fa08aa68f3932f9a2d9628d67494a96ef1a20b46
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-02-02 11:19:13 -0300

    Use ReferenceTests.jl

--- test/criminisi.jl
+++ test/criminisi.jl
@@ -1,12 +1,12 @@
 @testset "Crimisini" begin
   if visualtests
-    # Diagonal
+    # diagonal pattern
     img  = [i + j > 10 ? 0.1 : 0.2 for i in 1:10, j in 1:10]
     mask = [1.2cos(i) + i > j for i in 1:10, j in 1:10]
     out  = inpaint(img, mask, Criminisi(5,5))
-    @plottest plot_before_after(img,mask,out) joinpath(datadir,"Diagonal.png") !istravis
+    @test_reference "data/diagonal.txt" out
 
-    # Cross
+    # cross pattern
     img = zeros(50,50) .+ 0.5
     for i in 1:50
       img[i,i] = 1.0
@@ -15,20 +15,20 @@
     mask = falses(size(img))
     mask[20:30,15:40] .= true
     out = inpaint(img, mask, Criminisi(9,9))
-    @plottest plot_before_after(img,mask,out) joinpath(datadir,"Cross.png") !istravis
+    @test_reference "data/cross.txt" out
 
-    # Blobs
+    # blobs pattern
     img  = Float64.(Gray.(testimage("blobs")))
     mask = falses(size(img))
     mask[50:150,50:150] .= true
     out = inpaint(img, mask, Criminisi(11,11))
-    @plottest plot_before_after(img,mask,out) joinpath(datadir,"Blobs.png") !istravis
+    @test_reference "data/blobs.png" out
 
-    # LightHouse
+    # lighthouse image
     img  = Float64.(Gray.(testimage("lighthouse")))
     mask = falses(size(img))
     mask[50:350,300:400] .= true
     out = inpaint(img, mask, Criminisi(30,30))
-    @plottest plot_before_after(img,mask,out) joinpath(datadir,"LightHouse.png") !istravis
+    @test_reference "data/lighthouse.png" out
   end
 end
